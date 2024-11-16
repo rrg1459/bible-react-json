24b01600646618d2bd1d50e99da79dd9e48f9e5d
rafaDev <rafa.rodriguez@skydropx.com>
2024-11-16 19:52:56 -0400

    feat: change name of variables

--- src/pages/numVerses/NumVerses.jsx
+++ src/pages/numVerses/NumVerses.jsx
@@ -30,7 +30,7 @@ const NumVerses = (props) => {
     <div className="num-verses">
       <div className="main">
         <GridContainer column={column} row={row} className="grid-container">
-          {verses?.map((x) => <div key={x} className='number'>{x}</div>)}
+          {verses?.map((x) => <div key={x} className='versess'>{x}</div>)}
         </GridContainer>
       </div>
     </div>
