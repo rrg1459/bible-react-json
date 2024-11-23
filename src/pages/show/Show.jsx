c24318b6efc5578d84c8c2dbf97b3f2906e37459
rafaDev <rrg1459@hotmail.com>
2024-11-23 10:15:13 -0400

    feat: add useKeyPress and component

--- src/pages/show/Show.jsx
+++ src/pages/show/Show.jsx
@@ -1,6 +1,9 @@
 import { useSelector } from "react-redux";
+import { useState } from 'react';
+import { useKeyPress } from "@uidotdev/usehooks";
 import NumChapters from "../../components/numChapters/NumChapters.jsx"
 import NumVerses from "../../components/numVerses/NumVerses.jsx"
+import RefreshLanguage from "../../components/refreshLanguage/RefreshLanguage.jsx"
 import Books from "../../components/books/Books.jsx"
 import Verses from "../../components/verses/Verses.jsx"
 import './show.scss';
@@ -8,26 +11,45 @@ import './show.scss';
 const Show = () => {
 
   const selectQuote = (state) => state.quote;
-
   const { book, verses, chapter, verse } = useSelector(selectQuote);
 
+  const [activeKey, setActiveKey] = useState("");
+
+  const vowels = ['e', 'E', 'i', 'I'];
+
+  useKeyPress("e", onKeyPress);
+  useKeyPress("E", onKeyPress);
+  useKeyPress("i", onKeyPress);
+  useKeyPress("I", onKeyPress);
+
+  function onKeyPress(e) {
+    e.preventDefault();
+    setActiveKey(e.key);
+    setTimeout(() => {
+      setActiveKey("");
+    }, 0);
+  }
+
   return (
-    <div className="show">
-      <div className="parent">
-        <div className="verses">
-          <Verses />
-        </div>
-        <div className="books">
-          <Books />
-        </div>
-        <div className="num-chapters">
-          <NumChapters numChapters={book.chapters} chapter={chapter} />
-        </div>
-        <div className="num-verses">
-          <NumVerses numVerses={verses.length} verse={verse} />
+    <>
+    {(vowels.includes(activeKey)) && <RefreshLanguage activeKey={activeKey} />}
+      <div className="show">
+        <div className="parent">
+          <div className="verses">
+            <Verses />
+          </div>
+          <div className="books">
+            <Books />
+          </div>
+          <div className="num-chapters">
+            <NumChapters numChapters={book.chapters} chapter={chapter} />
+          </div>
+          <div className="num-verses">
+            <NumVerses numVerses={verses.length} verse={verse} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
 
   );
 };
